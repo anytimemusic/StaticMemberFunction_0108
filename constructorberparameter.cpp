a01b9a57c4571fb5d06f8fe5f9fb9814826a0c0d
anytimemusic <bima220711@gmail.com>
2026-05-05 18:55:19 +0700

    definisi constructor dgn parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -7,4 +7,9 @@ public:
     mahasiswa(int nim, string nama); //Constructor dengan parameter
 };
 
-
+// Definisi Constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor dengan parameter Terpanggil" << endl;
+    cout << "NIM: " << nim << endl;
+    cout << "Nama: " << nama << endl;
+}
