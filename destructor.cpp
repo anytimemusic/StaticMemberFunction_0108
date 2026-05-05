4bb992408ee76b209bb95b83115646ff7ed94760
anytimemusic <bima220711@gmail.com>
2026-05-05 19:18:10 +0700

    panggil constructor

--- destructor.cpp
+++ destructor.cpp
@@ -13,3 +13,43 @@ public:
     void cetakData();
     void isiData();
 };
+
+angka::angka(int i){ 
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka() {
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{
+    for (int i = 0; i <= panjang; i++)
+    {
+        cout << i << "=" << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 0; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    angka belajarcpp(3);                    // Constructor Terpanggil
+    angka *ptrBelajarcpp = new angka(5);    // Constructor Terpanggil
+    delete ptrBelajarcpp;                   // Destructor Terpanggil
+
+    return 0;
+}
